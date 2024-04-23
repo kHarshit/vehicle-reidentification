7615229a2c5685b5cc8e56a8eadd050bd7d8f3fb
Harshit Kumar <kumar.hars@northeastern.edu>
2024-04-23 17:28:17 -0400

    Restructure code

--- main.py
+++ main.py
@@ -14,20 +14,20 @@ from similarity import *
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", required=True, help="path to input video")
 parser.add_argument("-r", "--reference_img", required=True, help="path to reference image")
-parser.add_argument("-f", "--feature", default="hog", choices=["hog", "histogram", "dnn"],
+parser.add_argument("-f", "--feature", default="hog", choices=["hog", "histogram", "dnn", "composite"],
                     help="Feature type to use for comparison")
 args = parser.parse_args()
+
+# Similarity thresholds
+hog_threshold = 0.5
+histogram_threshold = 0.7
+cosine_threshold = 0.1
 
 # Initialize YOLO model
 model = YOLO("yolov8n.pt")
 cap = cv2.VideoCapture(args.input)
 
-if args.feature == "dnn":
-    # Load a pre-trained ResNet model
-    model_resnet = models.resnet50(pretrained=True)
-    model_resnet.eval()  # Set model to evaluation mode
-
-# Directory for saving images
+# Directory for saving images of matched vehicles
 save_dir = "images/"
 os.makedirs(save_dir, exist_ok=True)
 
@@ -37,21 +37,22 @@ reference_image = cv2.imread(reference_image_path)
 if reference_image is None:
     raise FileNotFoundError(f"Reference image at {reference_image_path} not found.")
 
-# Standardize image size for feature extraction
-standard_size = (128, 64)  # Typical size used for HOG feature extraction
-
-# Resize reference image
-reference_image = cv2.resize(reference_image, standard_size)
+if not args.feature == "dnn":
+    # Standardize image size for feature extraction
+    standard_size = (128, 64)  # Typical size used for HOG feature extraction
+    # Resize reference image
+    reference_image = cv2.resize(reference_image, standard_size)
 
 # Compute features for the reference image
-reference_hog_features = compute_hog_features(reference_image)
-reference_color_histogram = compute_color_histogram(reference_image)
-if args.feature == "dnn":
+if args.feature == "hog":
+    reference_hog_features = compute_hog_features(reference_image)
+elif args.feature == "histogram":
+    reference_color_histogram = compute_color_histogram(reference_image)
+elif args.feature == "dnn":
+    # Load a pre-trained ResNet model
+    model_resnet = models.resnet50(pretrained=True)
+    model_resnet.eval()  # Set model to evaluation mode
     reference_dnn_features = extract_dnn_features(model_resnet, reference_image)
-
-# Similarity thresholds (these values might need tuning based on your specific use case)
-hog_threshold = 0.5  # Adjust based on experimentation
-histogram_threshold = 0.2
 
 # get vehicle classes: bicycle, car, motorcycle, airplane, bus, train, truck, boat
 # class_list = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -73,24 +74,32 @@ while cap.isOpened():
         for i, box in enumerate(boxes):
             x1, y1, x2, y2 = map(int, box)  # Use xyxy format
             crop_img = frame[y1:y2, x1:x2]  # Use xyxy format
-            # save image
-            crop_name = f"frame_{frame_count}_ID_{results[0].boxes.id[i]}.jpg"
-            crop_path = os.path.join(save_dir, crop_name)
-            cv2.imwrite(crop_path, crop_img)
-            crop_img = cv2.resize(crop_img, standard_size)  # Resize to standard size
 
-            # Compute features for the detected vehicle
-            hog_features = compute_hog_features(crop_img)
-            color_histogram = compute_color_histogram(crop_img)
+            # save all image
+            # crop_name = f"frame_{frame_count}_ID_{results[0].boxes.id[i]}.jpg"
+            # crop_path = os.path.join(save_dir, crop_name)
+            # cv2.imwrite(crop_path, crop_img)
+
+            # resize image for non-DNN features
+            if not args.feature == "dnn":
+                crop_img = cv2.resize(crop_img, standard_size)  # Resize to standard size
+
             match = False
-            if args.feature == "dnn":
+            if args.feature == "hog":
+                # Compare HOG features
+                hog_features = compute_hog_features(crop_img)
+                l2_dist = l2_distance(hog_features, reference_hog_features)
+                match = l2_distance(hog_features, reference_hog_features) < hog_threshold
+            elif args.feature == "histogram":
+                # Compare color histograms
+                color_histogram = compute_color_histogram(crop_img)
+                hist_dist = histogram_distance(color_histogram, reference_color_histogram)
+                match = histogram_distance(color_histogram, reference_color_histogram) > histogram_threshold
+            elif args.feature == "dnn":
+                # Compare DNN features
                 dnn_features = extract_dnn_features(model_resnet, crop_img)
                 cosine_dist = cosine_distance(dnn_features, reference_dnn_features)
-                if cosine_dist < 0.1:
-                    match = True
-            else:
-                # Compare features
-                match =  compare_features(hog_features, reference_hog_features, color_histogram, reference_color_histogram)
+                match = cosine_dist < cosine_threshold
             
             if match:
                 # id
@@ -99,7 +108,12 @@ while cap.isOpened():
                 cv2.rectangle(annotated_frame, (x1, y1), (x2, y2), (0, 255, 0), 3)  # Use xyxy format
                 cv2.putText(annotated_frame, 'Matched', (x1, y2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 3)
 
-        cv2.imshow("YOLOv8 Tracking", annotated_frame)
+                crop_name = f"frame_{frame_count}_ID_{results[0].boxes.id[i]}_{args.feature}.jpg"
+                crop_path = os.path.join(save_dir, crop_name)
+                cv2.imwrite(crop_path, crop_img)
+
+
+        cv2.imshow(f"YOLOv8 Tracking with ReID Matching - {args.feature}", annotated_frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
